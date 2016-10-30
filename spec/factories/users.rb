9ae83dd59797c5c9a86382ad550979d959e20795
Masaya Nasu <tomato.wonder.life@gmail.com>
2016-10-31 00:33:47 +0900

    add users spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    password { "hogehoge" }
+    password_confirmation { "hogehoge" }
   end
 end
