1a4da2db917a1fcd9cdae9977d2992b703d5e9ba
Masaya Nasu <tomato.wonder.life@gmail.com>
2016-10-30 18:41:03 +0900

    fix spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root :to => 'users#index'
   resources :user_sessions, only: [ :create, :destroy ]
-  resources :users, defaults: { format: 'html' }
+  resources :users
 
   get 'login' => 'user_sessions#new', :as => :login
   post 'logout' => 'user_sessions#destroy', :as => :logout
