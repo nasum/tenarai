6b64b4b5488b49ddabc735f757c6287a6e260cc0
Masaya Nasu <tomato.wonder.life@gmail.com>
2016-11-08 20:46:04 +0900

    fix admin root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root :to => 'home#index'
+  root to: 'home#index'
   resource :home, only: :index
 
   namespace :admin do
-    resources :home, only: :index
+    root to: 'home#index'
     resources :users
     resource :user_sessions, only: [ :new, :create, :destroy ]
   end
