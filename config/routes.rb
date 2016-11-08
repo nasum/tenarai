08c704db2a87a3d2d6dd891add7a8172ee641cab
Masaya Nasu <tomato.wonder.life@gmail.com>
2016-11-09 01:39:30 +0900

    fix admin page design

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   root to: 'home#index'
   resource :home, only: :index
 
+  get "admin" => 'admin/home#index'
+
   namespace :admin do
-    root to: 'home#index'
     resources :users
     resource :user_sessions, only: [ :new, :create, :destroy ]
   end
