77dc7fd06aec16c1558e04c199e2083c7c5805fd
nasum <tomato.wonder.life@gmail.com>
2016-11-16 23:10:57 +0900

    fix user_session test

--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Admin::UserSessionsController, type: :controller do
+RSpec.describe UserSessionsController, type: :controller do
 
   describe "GET #new" do
     it "returns http success" do
