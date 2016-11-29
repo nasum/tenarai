0c5985753ee460e5f78e377cc1aaa4d19eab2d9d
nasum <tomato.wonder.life@gmail.com>
2016-11-29 20:50:15 +0900

    fix design article

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   skip_before_action :require_login, only: :index
 
   def index
-    @articles = Article.page(1).per(10).order("created_at DESC")
+    @articles = Article.includes(:user).page(1).per(10).order("created_at DESC")
   end
 end
