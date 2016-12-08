e75ce0096b5bb28bf48ea3963cf88a2ef7327c82
nasum <tomato.wonder.life@gmail.com>
2016-12-08 21:27:16 +0900

    fix test

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,11 +1,13 @@
 FactoryGirl.define do
   factory :article, class: Article do
+    title { Faker::Lorem.sentence }
     content { Faker::Lorem.sentence }
     status { 0 }
     user { FactoryGirl.create :user }
   end
 
   factory :published_article, class: Article do
+    title { Faker::Lorem.sentence }
     content { Faker::Lorem.sentence }
     status { 1 }
     user { FactoryGirl.create :user }
