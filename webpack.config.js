147d8d0e1d5ac647d72f79f3005c7229b7611112
nasum <tomato.wonder.life@gmail.com>
2016-12-12 22:40:41 +0900

    add webpack.config.js and yarn.lock

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const webpack = require("webpack");
+
+module.exports = {
+  context: __dirname,
+  entry: {
+    app: "./frontend/src/app.js",
+  },
+  output: {
+    path: __dirname + "/frontend/dist",
+    filename: "[name].bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue'
+      },
+      {
+        test: /\.s[a|c]ss$/,
+        loader: 'style!css!sass'
+      }
+    ],
+  },
+  resolveLoader: {
+    modules: ['node_modules', __dirname + '/node_modules'],
+  }
+};
