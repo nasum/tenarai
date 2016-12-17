2cbc4c9c1c0df2b1405c59c343473c80e8c6c881
nasum <tomato.wonder.life@gmail.com>
2016-12-17 16:53:08 +0900

    add markdown preview

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
   resources :users, except: :index
   get 'signin', to: 'signin#new'
   resource :signin, controller: :signin, only: [ :create, :destroy ]
-  resources :articles
+  resources :articles do
+    collection do
+      post 'preview'
+    end
+  end
   scope path: :'@:user_name', constraints: { user_name: /.*/ } do
     resources :user_articles, path: 'articles', only: [:index, :show]
   end
