48684de021a07ad2901283cbb766655f2fd82814
nasum <tomato.wonder.life@gmail.com>
2017-01-18 19:30:03 +0900

    fix none search query result

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,16 +2,10 @@ class HomeController < ApplicationController
   skip_before_action :require_login, only: :index
 
   def index
-    @articles = Article.includes(:user).search(search_params).page(1).per(10).records
-  end
-
-  private
-
-  def search_params
-    if params[:search]
-      params[:search]
+    @articles = if params[:search]
+      Article.includes(:user).search(params[:search]).page(1).per(10).records
     else
-      '*'
+      Article.includes(:user).page(1).per(10)
     end
   end
 end
